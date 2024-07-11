dc30cae61516853b7e01dbb4c5d83151eb53d8af
CatsAndPrograming <milicoding2409ytandcats@gmail.com>
2024-07-11 09:12:01 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,89 @@
+const cacheName = 'catch_the_apple';
+const precachedResources = [
+  '/',
+  './js/animation.js',
+  './js/class.js',
+  './js/images.js',
+  './css/main.css',
+  './index.js',
+  './manifest.json',
+  './sw.js',
+  './img/apple.png', './img/apple_tree.png', './img/apple_worm.png', './img/arrow.png', './img/arrow2.png', './img/basket.png', './img/basket_flip.png', './img/dirt.png', './img/favicon.ico', './img/fullscreen.png', './img/pause.png', './img/play.png', './img/tree_1.png', './img/tree_2.png', './img/tree_3.png', './img/unfullscreen.png'
+];
+
+
+async function precache() {
+  const cache = await caches.open(cacheName);
+  return cache.addAll(precachedResources);
+}
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(precache());
+});
+
+async function cacheFirst(request) {
+  const cachedResponse = await caches.match(request);
+  if (cachedResponse) {
+    return cachedResponse;
+  }
+  try {
+    const networkResponse = await fetch(request);
+    if (networkResponse.ok) {
+      const cache = await caches.open(cacheName);
+      cache.put(request, networkResponse.clone());
+    }
+    return networkResponse;
+  } catch (error) {
+    return Response.error();
+  }
+}
+
+self.addEventListener("fetch", (event) => {
+  if (precachedResources.includes(url.pathname)) {
+    event.respondWith(cacheFirst(event.request));
+  }
+});
+
+function isCacheable(request) {
+  const url = new URL(request.url);
+  return !url.pathname.endsWith(".json");
+}
+
+async function cacheFirstWithRefresh(request) {
+  const fetchResponsePromise = fetch(request).then(async (networkResponse) => {
+    if (networkResponse.ok) {
+      const cache = await caches.open(cacheName);
+      cache.put(request, networkResponse.clone());
+    }
+    return networkResponse;
+  });
+
+  return (await caches.match(request)) || (await fetchResponsePromise);
+}
+
+self.addEventListener("fetch", (event) => {
+  if (isCacheable(event.request)) {
+    event.respondWith(cacheFirstWithRefresh(event.request));
+  }
+});
+
+async function networkFirst(request) {
+  try {
+    const networkResponse = await fetch(request);
+    if (networkResponse.ok) {
+      const cache = await caches.open(cacheName);
+      cache.put(request, networkResponse.clone());
+    }
+    return networkResponse;
+  } catch (error) {
+    const cachedResponse = await caches.match(request);
+    return cachedResponse || Response.error();
+  }
+}
+
+self.addEventListener("fetch", (event) => {
+  const url = new URL(event.request.url);
+  if (url.pathname.match(/^\/inbox/)) {
+    event.respondWith(networkFirst(event.request));
+  }
+});
